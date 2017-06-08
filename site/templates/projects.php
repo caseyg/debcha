8dbf24429c6e1deea172aca1205f22c2aacd97ac
Casey Gollan <casey@bullshit.systems>
2017-06-07 20:24:15 -0400

    content and template reorg

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -10,10 +10,14 @@
               <h2>
                 <a href="<?php echo $p->url() ?>"><?php echo $p->title() ?></a>
               </h2>
+              <?php if ($p->tags()->isNotEmpty()): ?>
               <?php foreach ($p->tags()->split($separator = ',') as $t): ?>
                 <span class="ml-3 <?php echo strtolower($t) ?>" title="<?php echo $t ?>">#<?php echo $t ?></span>
               <?php endforeach; ?>
+            <?php endif; ?>
+            <?php if ($p->subtitle()->isNotEmpty()): ?>              
               <?php echo $p->subtitle() ?>
+            <?php endif; ?>
           </li>
         <?php endforeach; ?>
       </ul>

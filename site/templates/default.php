ee2bffbdb3036debba098840eed2758172a06096
Casey Gollan <casey@bullshit.systems>
2017-07-27 12:42:36 -0400

    building out kirby

--- site/templates/default.php
+++ site/templates/default.php
@@ -7,9 +7,22 @@
       <h1><?php echo $page->title() ?></h1>
       <h2><?php echo $page->subtitle() ?></h2>
 
+      <?php if ($page->text()->isNotEmpty()): ?>
+        <?php echo $page->text()->kt() ?>
+      <?php endif; ?>
+
       <?php foreach($page->builder()->toStructure() as $section): ?>
         <?php snippet('sections/' . $section->_fieldset(), array('data' => $section)) ?>
       <?php endforeach ?>
+
+      <?php if ($page->tags()->isNotEmpty()): ?>
+        <?php foreach ($page->tags()->split($separator = ',') as $t): ?>
+          <a href="<?php echo url('../' . $page->url() . '/' . url::paramsToString(['tag' => $t])) ?>">
+            #<?php echo html($t) ?>
+          </a>
+        <?php endforeach; ?>
+      <?php endif; ?>
+
     </div>
   </div>
 </div>
--- site/templates/projects.php
+++ site/templates/projects.php
@@ -2,28 +2,54 @@
 
 <div class="container my-3">
   <div class="row my-3">
-    <div class="col-6">
-      <h2><?php echo $page->title() ?></h2>
-      <ul class="list-unstyled">
-        <?php foreach ($page->children() as $p): ?>
-          <li class="mb-4">
-              <a href="<?php echo $p->url() ?>">
-                <h3><?php echo $p->title() ?></h3>
+    <div class="col">
+      <h2 class="mb-4"><?php echo $page->title() ?></h2>
+
+      <?php if ($tag = param('tag')): ?>
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+          <a class="close" href="/projects">
+            <span aria-hidden="true">&times;</span>
+          </a>
+          Viewing <?php echo html($projects->count()) ?> project(s) tagged "<?php echo html($tag) ?>"
+        </div>
+      <?php endif; ?>
+
+      <div class="card-columns">
+        <?php foreach ($projects as $p): ?>
+          <div class="card mb-3">
+            <div class="card-block">
+
+                <h4 class="card-title"><a class="text-muted" href="<?php echo $p->url() ?>"><?php echo $p->title() ?></a></h4>
                 <?php if ($p->subtitle()->isNotEmpty()): ?>
-                  <h4><?php echo $p->subtitle() ?></h4>
+                  <p class="card-text"><?php echo $p->subtitle() ?></p>
                 <?php endif; ?>
-              </a>
+
 
               <?php if ($p->tags()->isNotEmpty()): ?>
-                <?php foreach ($p->tags()->split($separator = ',') as $t): ?>
-                  <a href="<?php echo url($page->url() . '/' . url::paramsToString(['tag' => $t])) ?>">
-                    #<?php echo html($t) ?>
-                  </a>
-                <?php endforeach; ?>
+                <p class="card-text">
+                  <small>
+                    <?php foreach ($p->tags()->split($separator = ',') as $t): ?>
+                      <a class="text-muted mr-1" href="<?php echo url($page->url() . '/' . url::paramsToString(['tag' => $t])) ?>">
+                        #<?php echo html($t) ?>
+                      </a>
+                    <?php endforeach; ?>
+                  </small>
+                </p>
               <?php endif; ?>
-          </li>
+            </div>
+          </div>
         <?php endforeach; ?>
-      </ul>
+      </div>
+
+      <?php if ($tag = param('tag')): ?>
+        <?php if ($news->count() > 0): ?>
+          <hr>
+          <h3 class="mb-4">Related News</h3>
+          <?php foreach ($news as $p): ?>
+            <a href="<?php echo $p->url() ?>"><?php echo $p->title() ?></a>, <?php if ($p->dates()->isNotEmpty()): ?><?php echo $p->dates() ?><?php endif; ?>
+          <?php endforeach; ?>
+        <?php endif; ?>
+      <?php endif; ?>
     </div>
   </div>
 </div>

60f02cdb72202e55b3626608219261760f8eaa66
Casey Gollan <casey@bullshit.systems>
2017-09-27 12:24:28 -0400

    instantclick

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -2,5 +2,7 @@
     </footer>
     <script src="https://use.typekit.net/ilk3lvh.js"></script>
     <script>try{Typekit.load({ async: true });}catch(e){}</script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/instantclick/3.0.1/instantclick.min.js" data-no-instant></script>
+    <script data-no-instant>InstantClick.init();</script>
   </body>
 </html>

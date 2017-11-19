fef62e5c09ce74c93a4511d10160ebcda5a13dae
Casey Gollan <casey@bullshit.systems>
2017-11-19 13:25:48 -0500

    adjust text and show images

--- site/templates/waypoint.php
+++ site/templates/waypoint.php
@@ -1,40 +1,47 @@
 <?php snippet('header') ?>
 
-<div class="container my-3">
-  <div class="row my-3">
-    <div class="col-12">
+    <div class="container my-3 text-center">
+      <div class="row my-3">
+        <div class="col-12">
 
-      <div class="waypoint__marker" style="background-color:<?php echo $page->color() ?>;">
-        <div class="waypoint__hole"></div>
+          <div class="waypoint__marker" style="background-color:<?php echo $page->color() ?>;">
+            <div class="waypoint__hole"></div>
+          </div>
+
+          <h1><?php echo $page->title() ?></h1>
+          <h2><?php echo $page->subtitle() ?></h2>
+        </div>
       </div>
-
-      <h1><?php echo $page->title() ?></h1>
-      <h2><?php echo $page->subtitle() ?></h2>
-
-      <?php if ($page->text()->isNotEmpty()): ?>
-        <?php echo $page->text()->kt() ?>
-      <?php endif; ?>
-
-      <?php foreach($page->builder()->toStructure() as $section): ?>
-        <?php snippet('sections/' . $section->_fieldset(), array('data' => $section)) ?>
-      <?php endforeach ?>
-
-      <?php if ($page->tags()->isNotEmpty()): ?>
-        <?php foreach ($page->tags()->split($separator = ',') as $t): ?>
-          <a href="<?php echo url($page->parent()->uri() . '/' . url::paramsToString(['tag' => $t])) ?>">
-            #<?php echo html($t) ?>
-          </a>
-        <?php endforeach; ?>
-      <?php endif; ?>
       <div class="row">
-        <?php foreach ($page->siblings()->visible()->flip() as $waypoint): ?>
-          <a class="col text-center" href="<?php echo $waypoint->url() ?>" style="color:<?php echo $waypoint->color() ?>;">
-            <div style="background-color:<?php echo $waypoint->color() ?>;" class="waypoint__marker waypoint__marker--small">
+        <?php if ($page->text()->isNotEmpty()): ?>
+          <div class="lead col-md-8 offset-md-2">
+            <?php echo $page->text()->kt() ?>
+
+            <?php if ($page->image()): ?>
+              <figure>
+                <img src="<?php echo $page->image()->url() ?>" alt="<?php echo $page->image()->caption() ?> <?php if ($page->image()->credit()): ?>(<?php echo $page->image()->credit() ?>)<?php endif; ?>">
+                <?php if ($page->image()->caption() or $page->image()->credit()): ?>
+                  <figcaption>
+                    <?php if ($page->image()->credit()->isNotEmpty()): ?>
+                      <small class="d-block text-right"><?php echo $page->image()->credit()->kt() ?></small>
+                    <?php endif; ?>
+                    <?php echo $page->image()->caption()->kt() ?>
+                  </figcaption>
+                <?php endif; ?>
+              </figure>
+            <?php endif; ?>
+          </div>
+        <?php endif; ?>
+      </div>
+      <div class="row">
+        <?php if($page->hasNextVisible()): ?>
+          <a class="col text-center" href="<?php echo $page->nextVisible()->url() ?>" style="color:<?php echo $page->nextVisible()->color() ?>;">
+            <div style="background-color:<?php echo $page->nextVisible()->color() ?>;" class="waypoint__marker waypoint__marker--small">
               <div class="waypoint__hole"></div>
             </div>
-            <?php echo $waypoint->title() ?>
+            <?php echo $page->nextVisible()->title() ?>
           </a>
-        <?php endforeach; ?>
+        <?php endif ?>
       </div>
     </div>
   </div>

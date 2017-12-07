669204318b9f57dcfeff5e46a61916f28f9a9ff5
Casey Gollan <casey@bullshit.systems>
2017-12-07 14:23:15 -0500

    section prototyping

--- site/templates/section.php
+++ site/templates/section.php
@@ -2,8 +2,8 @@
 <div class="container-fluid" style="background:#BBA164;">
   <div class="container py-3">
     <div class="row py-3">
-      <div class="col-12">
-        <small>Sections</small>
+      <div class="col">
+        <small>SECTIONS: <strong>Gender and inclusivity</strong> / Engineering education / Materials science / Technology and culture</small>
         <hr>
         <h1 class="pt-3" style="color:#fff;font-weight:900;">Gender, inclusivity and diversity</h1>
         <h2 class="pb-3" style="color:#fff;font-weight:100;">in engineering and beyond</h2>
@@ -13,12 +13,150 @@
 </div>
 <div class="container my-3">
   <div class="row my-3">
-    <div class="col-12">
+    <div class="col-8 offset-2">
       <p class="lead">
         Issues around inclusivity and diversity, particular as they relate to gender and STEM (science, technology, engineering and mathematics) have been a running theme in my professional work.
       </p>
       <p>It begins, of course, with my own experiences in engineering (including being an undergraduate student at the time of the Ecole Polytechnique shootings), and carries through in my work with students at Olin College, which has had an institutional commitment to gender parity since its very first class. It’s an ongoing theme of my research in engineering education, and my colleagues and I regularly run workshops on gender and STEM for engineering and other educators. I’ve also written, spoken, and consulted on gender to broader audiences, ranging from writing for the comic book Bitch Planet and for the scientific journal Nature, to speaking at the SXSW Interactive Conference, to consulting work with the senior leadership of an advertising agency.</p>
+    </div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="alert alert-light text-center col lead">
+      <figure>
+        <img src="/assets/Collaboratory banner.jpg" alt="">
 
+      </figure>
+      For more of my work around gender and STEM, <a href="/news/tag:gender">click here</a>.
+    </div>
+    <div class="alert alert-warning col mx-3 text-center p-3 lead"><br>If your organization is interested in working with me as a consultant, please send me an&nbsp;e-mail:<br><br> <a href="mailto:debbie.chachra@olin.edu" class="btn btn-lg btn-primary">✉️&nbsp;&nbsp;debbie.chachra@olin.edu</a><br><br></div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="col">
+      <h2>Writing</h2>
+    </div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="col offset-2">
+      <figure>
+        <img src="/assets/bitchplanet-comp.jpg" alt="">
+      </figure>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col offset-2 alert-light">
+      <p>&rarr;<br>I write a regular column, “Reinvention”, for the American Society for Engineering Education’s Prism magazine.<br>
+      </p>
+      <a href="/projects/reinvention" class="btn btn-outline-primary">Read more "Reinvention" columns</a>
+    </div>
+    <div class="col">
+      <a href="http://www.asee-prism.org/reinvention-feb/" style="color:#000;">
+        <img src="/assets/asee-logo.png" width="100px" alt="" class="mb-3">
+        <h3>Partway to Gender Parity</h3>
+        <blockquote style="color:#000;">
+          We now share the data showing gender differences with students as they start their projects, and ask them to think about why they believe those disparities occurred.
+        </blockquote>
+        <small>February 2014</small>
+      </a>
+    </div>
+
+    <div class="col">
+      <a href="https://www.nature.com/news/to-reduce-gender-biases-acknowledge-them-1.22502" style="color: #000;">
+        <img src="/assets/nature-logo.jpg" width="100px" alt="" class="mb-3">
+        <h3>To reduce gender biases, acknowledge them</h3>
+        <p>A former Google engineer’s memo on diversity reveals psychological blind spots, not biological differences, says Debbie Chachra.</p>
+        <small>22 August 2017</small>
+      </a>
+    </div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="col">
+      <h2>Women at Olin College</h2>
+    </div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="col offset-2">
+      <a href="http://www.wbur.org/news/2014/05/27/women-engineers-olin-college" class="text-secondary">
+        <h3 class="mb-3">“Why women don’t study engineering--and what one Mass. college is doing about it”</h3>
+        <figure><img src="/assets/0527_olin-1000x645.jpg"></figure>
+        <img src="/assets/wbur-logo.svg" width="100px" alt="" class="mb-3">
+      </a>
+    </div>
+    <div class="col">
+      <a href="http://www.wbur.org/edify/2017/11/06/olin-college-women-engineers" class="text-secondary">
+        <h3 class="mb-3">“At one college, women engineers help reshape the machine shop”</h3>
+        <figure><img src="/assets/1106_olin-machine-shop03-1000x666.jpg"></figure>
+        <img src="/assets/wbur-logo.svg" width="100px" alt="" class="mb-3">
+      </a>
+    </div>
+  </div>
+  <hr>
+  <h2>Research</h2>
+  <hr>
+  <div class="row">
+    <div class="col offset-2">
+      <a href="https://peer.asee.org/exploring-gender-and-self-confidence-in-engineering-students-a-multi-method-approach" class="text-secondary">
+        <h3>Exploring Gender and Self-Confidence in Engineering Students: a Multi-Method Approach</h3>
+        <p>Despite generally higher academic achievement, female students display lower academic self- confidence than males. Of particular interest to engineering educators is the difference in confidence in mathematical or scientific skills. Gender differences among engineering students have been explored by the Persistence in Engineering survey, a component of the Academic Pathways Study (APS), a multi-method longitudinal study...</p>
+      </a>
+      <a href="#" class="btn btn-outline-primary">Download Paper (PDF)</a>
+    </div>
+    <div class="col">
+      <a href="https://peer.asee.org/interactive-panel-improving-the-experiences-of-marginalized-students-on-engineering-design-teams" class="text-secondary">
+        <h3>Improving the Experiences of Marginalized Students on Engineering Design Teams</h3>
+        <p>Interactive Panel: Improving the Experiences of Marginalized Students on Engineering Design TeamsIt is widely recognized that collaborative learning (i.e., group work) benefits student learning bycreating an educational environment in which students achieve higher grades, learn at a deeperlevel and retain information longer. However, group work is fraught with challenges...</p>
+      </a>
+      <a href="#" class="btn btn-outline-primary">Download Paper (PDF)</a>
+    </div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="col">
+      <h2>Speaking</h2>
+    </div>
+  </div>
+  <hr>
+
+  <div class="row">
+    <div class="col offset-2">
+      <a href="https://livestream.com/TheNewSchool/mattering-by-design/videos/83571440" class="text-secondary">
+        <figure>
+          <h3>Gender Differences and Design</h3>
+          <h4>Matter(ing) by Design symposium, The New School</h4>
+
+          <img src="/assets/newschool.png" alt="">
+
+          <small class="mt-3">(Presentation starts at 26:30)</small>
+
+        </figure>
+      </a>
+    </div>
+  </div>
+
+  <hr>
+  <div class="row">
+    <div class="alert alert-warning col mx-3 text-center p-3 lead"><br>If your organization is interested in working with me as a consultant, please send me an&nbsp;e-mail:<br><br> <a href="mailto:debbie.chachra@olin.edu" class="btn btn-lg btn-primary">✉️&nbsp;&nbsp;debbie.chachra@olin.edu</a><br><br></div>
+    <div class="alert alert-light text-center col lead">
+      <figure>
+        <img src="/assets/Collaboratory banner.jpg" alt="">
+
+      </figure>
+      For more of my work around gender and STEM, <a href="/news/tag:gender">click here</a>.
+    </div>
+  </div>
+
+</div>
+<div class="container-fluid" style="background:#BBA164;">
+  <div class="container py-3">
+    <div class="row py-3">
+      <div class="col">
+        <span style="color:white;">More Sections:</span><br>
+        <small><strong>Gender and inclusivity</strong> / Engineering education / Materials science / Technology and culture</small>
+      </div>
     </div>
   </div>
 </div>

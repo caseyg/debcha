bbd15d72994d82530328519c03010c930bf0cf7f
Casey Gollan <casey@bullshit.systems>
2017-12-29 09:51:30 -0500

    change nav to "Debbie Chachra, PhD", fixes #42

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -12,7 +12,7 @@
 
 <header>
     <nav id="topnav" class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="/"><?php echo $site->title() ?></a>
+      <a class="navbar-brand" href="/"><strong>Debbie Chachra</strong>, PhD</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>

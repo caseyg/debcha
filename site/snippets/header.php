bc89245ac16e585990bbe78049b6bdf1dc7d6a80
Casey Gollan <casey@bullshit.systems>
2017-12-29 09:48:29 -0500

    use local css/js

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <title><?php echo $page->title() ?> | Deb Chachra</title>
     <meta name="description" content="<?php echo str::unhtml($site->description()) ?>">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <?php echo css('assets/css/index.css') ?>
+    <?php echo css('/assets/css/bootstrap.min.css') ?>
+    <?php echo css('/assets/css/index.css') ?>
   </head>
 <body id="<?php echo $page->slug() ?>" class="parent-<?php echo $page->parent()->slug() ?>">
 

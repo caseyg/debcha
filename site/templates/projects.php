0a8b814e06607ea36406ef78310f3c3b7b010f15
Casey Gollan <casey@bullshit.systems>
2018-01-08 13:58:02 -0500

    bad design pass on projects style

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -1,10 +1,10 @@
 <?php snippet('header') ?>
 
-<div class="container my-3">
-  <div class="row my-3">
-    <div class="col">
-      <h1 class="mb-4"><?php echo $page->title() ?></h1>
-
+<div class="container py-3">
+  <div class="row py-3">
+    <div class="col-12">
+      <h1 class="pt-3"><?php echo $page->title() ?></h1>
+      <hr>
       <?php if ($tag = param('tag')): ?>
         <div class="alert alert-warning alert-dismissible fade show" role="alert">
           <a class="close" href="/projects">
@@ -13,12 +13,18 @@
           Viewing <?php echo html($projects->count()) ?> project(s) tagged "<?php echo html($tag) ?>"
         </div>
       <?php endif; ?>
+    </div>
+  </div>
+</div>
 
+<div class="container my-3">
+  <div class="row my-3">
+    <div class="col">
       <div class="card-columns">
         <?php foreach ($projects as $p): ?>
           <div class="card mb-3">
             <div class="card-block">
-                <h4 class="card-title"><a href="<?php echo $p->url() ?>"><?php echo $p->title() ?></a></h4>
+                <h4 class="card-title pt-3 mb-1"><a href="<?php echo $p->url() ?>"><?php echo $p->title() ?></a></h4>
                 <?php if ($p->subtitle()->isNotEmpty()): ?>
                   <p class="card-text"><?php echo $p->subtitle() ?></p>
                 <?php endif; ?>
@@ -28,7 +34,7 @@
                 <?php endif; ?>
 
               <?php if ($p->tags()->isNotEmpty()): ?>
-                <p class="card-text">
+                <p class="card-text pb-3">
                   <small>
                     <?php foreach ($p->tags()->split($separator = ',') as $t): ?>
                       <a class="text-muted mr-1" href="<?php echo url($page->url() . '/' . url::paramsToString(['tag' => $t])) ?>">

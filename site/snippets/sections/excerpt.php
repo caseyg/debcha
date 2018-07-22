291fb209ce31ab43c4204e813d08c21a2d9f3833
Casey Gollan <casey@bullshit.systems>
2018-07-21 23:00:19 -0400

    styling excerpts

--- site/snippets/sections/excerpt.php
+++ site/snippets/sections/excerpt.php
@@ -1,8 +1,9 @@
-<section class="excerpt">
-  <blockquote>
+<hr style="border-color: <?= $page->color(); ?>">
+<section class="excerpt" style="color: <?= $page->color(); ?>">
+  <div class="citation">
+    <?= $data->date_text() ?>: <a href="<?= $data->link() ?>"><strong><?= $data->title() ?></a></strong>
+  </div>
+  <blockquote class="mt-3" style="color: <?= $page->color(); ?>; border-color: <?= $page->color(); ?>;">
     <?= $data->text() ?>
   </blockquote>
-  <div class="citation">
-    <a href="<?= $data->link() ?>"><?= $data->title() ?></a>, <?= $data->date_text() ?>
-  </div>
 </section>

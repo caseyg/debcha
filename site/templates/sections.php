3798b79a53cd41701d99f2ad37ca95e187b836a1
Casey Gollan <casey@bullshit.systems>
2018-07-21 21:49:13 -0400

    format sections landing

--- site/templates/sections.php
+++ site/templates/sections.php
@@ -1,21 +1,20 @@
 <?php snippet('header') ?>
 <?php snippet('nav') ?>
 
+<section class="home__sections mt-1">
 <div class="container my-3">
+  <h1><?php echo $page->title() ?></h1>
   <div class="row my-3">
-    <div class="col-6">
-      <h1><?php echo $page->title() ?></h1>
-      <ol>
-        <?php foreach ($page->children() as $p): ?>
-          <li>
-              <h3>
-                <a href="<?php echo $p->url() ?>"><?php echo $p->title() ?></a>
-              </h3>
-          </li>
-        <?php endforeach; ?>
-      </ul>
+  <?php foreach ($site->find("sections")->children()->visible() as $section): ?>
+    <div class="col-12 p-3 m-t-3">
+      <a href="<?php echo $section->url() ?>" class="h3 card sections__card" style="background-color:<?php echo $section->color() ?>;">
+        <span class-"section__title"><?= $section->title() ?></span>
+        <span class="section__subtitle"><?= $section->subtitle() ?></span>
+      </a>
     </div>
-  </div>
+  <?php endforeach; ?>
+  </disv>
 </div>
+</section>
 
 <?php snippet('footer') ?>
